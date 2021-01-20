9f3338b1f111bcb474929e6d31b87a7f5ffa2d6b
prabhu-sunderaraman <prabhu.bits@gmail.com>
2021-01-20 15:07:21 +0530

    Day03

--- Day03/src/main/java/com/intuit/ExceptionHandling.java
+++ Day03/src/main/java/com/intuit/ExceptionHandling.java
@@ -1,16 +1,29 @@
 package com.intuit;
 
 import akka.actor.typed.ActorRef;
+import akka.actor.typed.ActorSystem;
 import akka.actor.typed.Behavior;
+import akka.actor.typed.SupervisorStrategy;
 import akka.actor.typed.javadsl.AbstractBehavior;
 import akka.actor.typed.javadsl.ActorContext;
 import akka.actor.typed.javadsl.Behaviors;
 import akka.actor.typed.javadsl.Receive;
 
+/*
+ * When an exception occurs in the Guardian actor, the application terminates
+ * When an exception occurs inside a child actor the actor is terminated by default. All the messages to this actor, fall into a DLQ
+ * 
+ * You can configure a SupervisorStrategy for the actors. Specify what to do on failure
+ * Wrap you actor's behavior with a supervisor and specify resume or restart or stop 
+ * */
+
 public class ExceptionHandling {
 
 	public static void main(String[] args) {
-
+		ActorSystem<String> parent = ActorSystem.create(Parent.create(), "parent");
+		parent.tell("add");
+		parent.tell("divide");
+		parent.tell("add");
 	}
 
 }
@@ -19,7 +32,9 @@ class Parent extends AbstractBehavior<String> {
 
 	public Parent(ActorContext<String> context) {
 		super(context);
-		myMathActor = context.spawn(MyMath.create(), "mymath");
+		//myMathActor = context.spawn(MyMath.create(), "mymath");
+		//myMathActor = context.spawn(Behaviors.supervise(MyMath.create()).onFailure(SupervisorStrategy.resume()), "mymath");
+		myMathActor = context.spawn(Behaviors.supervise(MyMath.create()).onFailure(SupervisorStrategy.restart()), "mymath");
 	}
 
 	private ActorRef<String> myMathActor;
@@ -49,19 +64,23 @@ class MyMath extends AbstractBehavior<String> {
 		super(context);
 	}
 
+	private String state = "INITIAL-101";
+	
 	@Override
 	public Receive<String> createReceive() {
 		return newReceiveBuilder()
 				.onMessageEquals("add", () -> {
+					state += "-ADD";
 					int num1 = 100;
 					int num2 = 100;
-					System.out.println("*** Sum is " + (num1 + num2) );
+					System.out.println("*** Sum is " + (num1 + num2) + " and state is " + state);
 					return this;
 				})
 				.onMessageEquals("divide", () -> {
+					state += "-DIVIDE";
 					int num1 = 100;
-					int num2 = 100;
-					System.out.println("*** Division is " + (num1 / num2) );
+					int num2 = 0;
+					System.out.println("*** Division is " + (num1 / num2) + " and state is " + state);
 					return this;
 				})
 				.build();
