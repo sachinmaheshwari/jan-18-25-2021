b4399b4f916bb4af654b63e5c17eaf76c6cd75be
prabhu-sunderaraman <prabhu.bits@gmail.com>
2021-01-22 15:05:36 +0530

    Day05

--- Day05/src/main/java/com/intuit/AskExample1.java
+++ Day05/src/main/java/com/intuit/AskExample1.java
@@ -1,18 +1,41 @@
 package com.intuit;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.concurrent.CompletionStage;
 
 import akka.actor.typed.ActorRef;
+import akka.actor.typed.ActorSystem;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.javadsl.AbstractBehavior;
 import akka.actor.typed.javadsl.ActorContext;
+import akka.actor.typed.javadsl.AskPattern;
 import akka.actor.typed.javadsl.Behaviors;
 import akka.actor.typed.javadsl.Receive;
 
 public class AskExample1 {
 
 	public static void main(String[] args) {
-
+		ActorSystem<Command> timeActor = ActorSystem.create(TimeBehavior.create(), "time-behavior");
+		
+		CompletionStage<String> completionStage = AskPattern.ask(timeActor, 
+				fabricatedHiddenActorRef -> {
+					Command cmd = new Command();
+					cmd.ref = fabricatedHiddenActorRef;
+					return cmd;
+				}, 
+				Duration.ofSeconds(2), 
+				timeActor.scheduler());
+		
+		completionStage.whenComplete((response, throwable) -> {
+			if(response != null) {
+				System.out.println(response);
+			}
+			else {
+				System.out.println("Oops! no response");
+			}
+		});
+		System.out.println("End of main");
 	}
 
 }
@@ -27,6 +50,7 @@ class TimeBehavior extends AbstractBehavior<Command> {
 	public Receive<Command> createReceive() {
 		return newReceiveBuilder()
 				.onAnyMessage(cmd -> {
+					Thread.sleep(5000);
 					cmd.ref.tell("It's " + LocalDateTime.now());
 					return this;
 				})
